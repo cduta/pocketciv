39dc1b487515d77fd76f6b8f43f8ab9e896724fe
RyogaU <RyogaU@googlemail.com>
2017-01-11 19:55:19 +0100

    - Some corrections for end of game instructions.

--- Instruction/EndGameInstruction.cpp
+++ Instruction/EndGameInstruction.cpp
@@ -7,20 +7,21 @@ EndGameInstruction::EndGameInstruction(BoardModel *boardModel)
 void EndGameInstruction::initInstruction()
 {
     this->boardModel->printMessage("This is the End of the Game...");
-    this->boardModel->printMessage(" ");
-    this->boardModel->printMessage("To start choosing, press Done.");
-    return;
-}
+    this->boardModel->printMessage("Add all VPs of the Wonders to the final Glory Score.");
 
-Instruction *EndGameInstruction::triggerDone()
-{
-    this->boardModel->printMessage(" ");
-    this->boardModel->printMessage("Adding all VPs of the Wonders to the final Glory Score.");
+    int wonderVP = 0;
     // TODO: Add the Wonders to the final glory score.
+
+    this->boardModel->printMessage(QString("Your VPs from Wonders is %1.").arg(wonderVP));
     this->boardModel->printMessage(" ");
     this->boardModel->printMessage(QString("Your final Glory Score is: %1 !").arg(this->boardModel->getGloryScore()));
     this->boardModel->printMessage(" ");
     this->boardModel->printMessage("THE GAME IS OVER.");
     this->boardModel->disableButtons();
+    return;
+}
+
+Instruction *EndGameInstruction::triggerDone()
+{
     return new NoInstruction();
 }
